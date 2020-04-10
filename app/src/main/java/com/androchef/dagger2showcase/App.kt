ad59c17fd2b44a6292a016039925a87619588e26
Happy Singh <happysingh23828@gmail.com>
2020-04-10 16:38:07 +0530

    Removed Manual dependency from this branch.

--- app/src/main/java/com/androchef/dagger2showcase/App.kt
+++ app/src/main/java/com/androchef/dagger2showcase/App.kt
@@ -4,12 +4,9 @@ import android.app.Application
 import com.androchef.dagger2showcase.dagger_di.components.AppComponent
 import com.androchef.dagger2showcase.dagger_di.components.DaggerAppComponent
 import com.androchef.dagger2showcase.dagger_di.modules.DriverModule
-import com.androchef.dagger2showcase.manual_di.containers.AppContainer
 
 class App : Application() {
 
-    //Manual Dependency
-//    val appContainer = AppContainer()
 
     private lateinit var appComponent: AppComponent
 
